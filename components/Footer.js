d9e79a5e6232689cd76b7083de23c1001b21571c
ProUnebit <prounebit@yandex.ru>
2023-04-01 00:01:47 +0300

    added Navbar comp

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Heading from "./Headiing";
 
 const Footer = () => (
   <footer>
-    <Heading tag="h3" text="Created by webDev" />
+    <Heading tag="h3" text="Footer INFO" />
   </footer>
 );
 
